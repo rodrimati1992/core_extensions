68cfdbfc18cebb7555d555ea02c6905d621489f0
rodrimati1992 <rodrimatt1985@gmail.com>
2020-04-06 10:41:55 -0300

    Fixed minimum versions,added minimum version build in CI.
    Bumped core_extensions patch version to 0.1.16

--- build.rs
+++ build.rs
@@ -4,7 +4,6 @@ use rustc_version::{version, Version};
 fn main() {
     let rver = version().unwrap();
 
-
     if Version::new(1, 22, 0) <= rver {
         println!("cargo:rustc-cfg=rust_1_22");
     }
