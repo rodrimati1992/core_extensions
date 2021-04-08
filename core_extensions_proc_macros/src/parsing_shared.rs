f3793de1a76899cdd9b86eb3dfb60efe92108fef
rodrimati1992 <rodrimatt1985@gmail.com>
2021-04-08 03:37:14 -0300

    Refactored proc macro, bumped versions to 1.2.0.
    Refactored proc macro crate so that a `split_impl` macro will be easier to write.

---
+++ core_extensions_proc_macros/src/parsing_shared.rs
@@ -0,0 +1,57 @@
+use crate::used_proc_macro::{
+    token_stream::IntoIter,
+    Delimiter, Group, Spacing, Span, TokenStream, TokenTree
+};
+
+
+use core::iter::once;
+
+use alloc::string::ToString;
+
+
+pub(crate) fn parenthesize_ts(ts: TokenStream, span: Span) -> TokenTree {
+    let mut group = Group::new(Delimiter::Parenthesis, ts);
+    group.set_span(span);
+    TokenTree::Group(group)
+}
+
+pub(crate) fn out_parenthesized(ts: TokenStream, span: Span, out: &mut TokenStream) {
+    out.extend(once(parenthesize_ts(ts, span)));
+}
+
+
+pub(crate) fn parse_path_and_args<F>(
+    macro_name: &str,
+    iter: &mut IntoIter,
+    f: F,
+) -> TokenStream 
+where
+    F: FnOnce(&mut TokenStream)
+{
+    let mut out = TokenStream::new();
+
+    loop {
+        match iter
+            .next()
+            .unwrap_or_else(|| panic!("{} expected more tokens", macro_name) )
+        {
+            TokenTree::Group(group) if group.delimiter() == Delimiter::None => {
+                out.extend(group.stream());
+            }
+            TokenTree::Group(group) => {
+                let mut args = group.stream();
+
+                f(&mut args);
+
+                let mut args = TokenTree::Group(Group::new(group.delimiter(), args));
+                args.set_span(group.span());
+                out.extend(once(args));
+
+                return out;
+            }
+            x => {
+                out.extend(once(x));
+            }
+        }
+    }
+}
